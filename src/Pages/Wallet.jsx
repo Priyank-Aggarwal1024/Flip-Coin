fca7a0791ed1f50a38b0deb3b844476ab214b949
Karanjot Singh <k.jsingh.12003@gmail.com>
2024-05-17 00:29:22 +0530

    test

--- src/Pages/Wallet.jsx
+++ src/Pages/Wallet.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { W, ada, atom, avax, binance, blockto, bnb, brave, btc, coinbase, cross, doge, ethereum, flash, icp, kazi, logo, math, metamask, mlogo, nimbi, opera, plus, polygon, reload, safepal, sch, shibh, stx, tick, tp, trust, uni, usdc, wallet98, wbtc, xrp } from '../assets'
-import Footer from '../components/Footer/Footer'
+import { W, ada, atom, avax, binance, blockto, bnb, brave, btc, coinbase, cross, discordIcon, doge, ethereum, flash, icp, kazi, logo, math, metamask, mlogo, nimbi, opera, plus, polygon, reload, safepal, sch, shibh, stx, telegramIcon, tick, tp, trust, twitterIcon, uni, usdc, wallet98, wbtc, xIcon, xrp } from '../assets'
 import '../App.css'
 import { useNavigate } from 'react-router-dom'
 
@@ -14,7 +13,7 @@ function Wallet() {
   const [cryptoData, setCryptoData] = useState([])
   const [loading, setLoading] = useState(true)
 
-  let newCryptoData = [{ src: stx, name: 'stacks', symbol: 'stx', amount: 458.2, token: 0.0025 }, { src: xrp, name: 'ripple', symbol: 'xrp', amount: 458.2, token: 0.058 }, { src: doge, name: 'Dogecoin', symbol: 'doge', amount: 458.2, token: 0.0025 }, { src: ada, name: 'cardano', symbol: 'ada', amount: 458.2, token: 0.0025 }, { src: icp, name: 'internet computer protocol', symbol: 'icp', amount: 458.2, token: 0.0025 }, { src: stx, name: 'stacks', symbol: 'stx', amount: 458.2, token: 0.0025 }, { src: avax, name: 'avalanche', symbol: 'avax', amount: 0.002, token: 0.0025 }, { src: wbtc, name: 'Wrapper Bitcoin', symbol: 'wbtc', amount: 458.2, token: 0.0025 }, { src: uni, name: 'uniswap', symbol: 'uni', amount: 458.2, token: 0.0025 }, { src: polygon, name: 'matic polygon', symbol: 'matic', amount: 458.2, token: 0.0025 }, { src: stx, name: 'stacks', symbol: 'stx', amount: 458.2, token: 0.0025 },]
+  let newCryptoData = [{ src: stx, name: 'stacks', symbol: 'stx', amount: 458.2, token: 0.0025 }, { src: xrp, name: 'ripple', symbol: 'xrp', amount: 458.2, token: 0.058 }, { src: doge, name: 'Dogecoin', symbol: 'doge', amount: 458.2, token: 0.0025 }, { src: icp, name: 'internet computer protocol', symbol: 'icp', amount: 458.2, token: 0.0025 }, { src: avax, name: 'avalanche', symbol: 'avax', amount: 0.002, token: 0.0025 }, { src: wbtc, name: 'Wrapper Bitcoin', symbol: 'wbtc', amount: 458.2, token: 0.0025 }, { src: uni, name: 'uniswap', symbol: 'uni', amount: 458.2, token: 0.0025 },]
 
   const activateNavbar = () => {
 
@@ -53,7 +52,7 @@ function Wallet() {
 
   const convert = () => {
     setConnected(false)
-    document.querySelector('.walletBalance').style.display ='none'
+    document.querySelector('.walletBalance').style.display = 'none'
   }
 
   const updateSlippage = (e) => {
@@ -68,17 +67,52 @@ function Wallet() {
     cryptoData.length === 0 ? setCryptoData(newCryptoData) : setCryptoData([])
   }
 
-  const walletUpdate = () => {
+  const [showWalletBalance, setShowWalletBalance] = useState(false);
+
+  const toggleWalletBalance = () => {
+    setTimeout(() => setShowWalletBalance(!showWalletBalance), 100);
     document.querySelector('.walletBalance').style.display = document.querySelector('.walletBalance').style.display === 'flex' ? 'none' : 'flex'
-    // window.addEventListener('click',(e)=>{
-    //   if(!document.querySelector('.walletBalance').contains(e.target)){
-    //     document.querySelector('.walletBalance').style.display='none'
-    //   }
-    // })
-  }
+  };
+
+  const handleOutsideClick = (e) => {
+    if (showWalletBalance && !document.querySelector('.walletBalance').contains(e.target)) {
+      setShowWalletBalance(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showWalletBalance) {
+      document.addEventListener('click', handleOutsideClick);
+      console.log('hi')
+    } else {
+      document.removeEventListener('click', handleOutsideClick);
+      // console.log('hi')
+    }
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [showWalletBalance]);
+
+  const walletUpdate = () => {
+    const walletBalance = document.querySelector('.walletBalance');
+    if (walletBalance.style.display === 'flex') {
+      walletBalance.style.display = 'none';
+      // Add event listener after changing display to 'none'
+      const handleClickOutside = (e) => {
+        if (!walletBalance.contains(e.target)) {
+          walletBalance.style.display = 'none';
+          window.removeEventListener('click', handleClickOutside);
+        }
+      };
+      window.addEventListener('click', handleClickOutside);
+    } else {
+      walletBalance.style.display = 'flex';
+    }
+  };
+
 
   return (
-    <div className='capitalize'>
+    <div className='capitalize relative'>
 
       <div className="fixed w-full">
         <nav className='lg:w-[80%] 2xl:w-[1504px] mx-auto border-b border-b-[#5c666c] bg-[#242d32] z-50 '>
@@ -112,7 +146,7 @@ function Wallet() {
                 }
                 {
                   connected &&
-                  <div onClick={walletUpdate} className='btn1 bg-transparent rounded-sm border border-[#5c666c] px-2 py-1.5  text-sm flex items-center gap-1'>
+                  <div onClick={toggleWalletBalance} className='btn1 bg-transparent rounded-sm border border-[#5c666c] px-2 py-1.5  text-sm flex items-center gap-1'>
                     <div className="w-4 h-4">
                       <img src={tick} alt="" className='w-full h-full object-cover' />
                     </div>
@@ -170,31 +204,32 @@ function Wallet() {
         </nav>
       </div>
 
-
-
       <div className="flex flex-col items-center gap-11 w-full px-[1rem] md:px-[5rem] xl:px-[11rem] mx-auto capitalize py-[6rem] bg-[url(./assets/mountain.png)] bg-bottom bg-cover min-h-screen bg-no-repeat font-['Roboto_Condensed',sans-serif]">
 
         <div className="flex flex-col items-center justify-center gap-1 lg:w-[75%] text-center">
           <p className='text-[30px] font-medium'>crypto dust converter</p>
-          <p className='text-sm text-center text-[#A1A7AA]'>convert small token amounts</p>
+          <p className='text-sm text-center text-[#A1A7AA] font-["Roboto"]'>convert small token amounts</p>
         </div>
 
         {
           !connected &&
-          <div className="bg-[#2e3a41] border border-[#5c666c] flex flex-col gap-2 rounded-md sm:w-[405px] h-[428px] px-4 py-3.5">
+          <div className="bg-[#2e3a41] border border-[#5c666c] flex flex-col gap-2 rounded-md sm:w-[433px] h-[456px] px-3.5 py-3.5">
 
-            <div className="grid grid-cols-4 place-items-center bg-[#242d32] p-4 rounded-sm sm:w-[375px] h-[343px] px-3.5 py-3.5 overflow-y-auto">
+            <div className="sm:w-[405px] h-[372px] px-3.5 bg-[#242d32] rounded-sm py-3.5 overflow-y-auto">
 
-              {
-                data?.map((crypto, index) => (
-                  <div key={index} className="flex flex-col gap-2 items-center text-center px-3 py-2.5">
-                    <div className="w-[2rem] h-[2rem]">
-                      <img src={crypto.src} className='w-full h-full object-cover' alt="" />
+              <div className="grid grid-cols-4 gap-1 place-items-center ">
+
+                {
+                  data?.map((crypto, index) => (
+                    <div key={index} className="flex flex-col gap-2 w-[90px] h-[86px] items-center text-center px-3 py-[11px]">
+                      <div className="w-[2rem] h-[2rem]">
+                        <img src={crypto.src} className='w-full h-full object-cover' alt="" />
+                      </div>
+                      <p className='uppercase text-sm font-medium text-[#a1a7aa] font-["Inter"]'>{crypto.name}</p>
                     </div>
-                    <p className='uppercase text-sm font-["Inter"]'>{crypto.name}</p>
-                  </div>
-                ))
-              }
+                  ))
+                }
+              </div>
 
             </div>
 
@@ -207,8 +242,8 @@ function Wallet() {
           </div>
         }
 
-        <div className="connectModal fixed top-0 backdrop-blur-[1.5px] h-screen w-screen z-[100] hidden justify-center items-center ">
-          <div className="modal bg-[#2e3a41] -mt-[12rem] 3xl:mt-[-14rem] flex flex-col w-[18.75rem] h-[20rem] border border-[#5c666c] px-4 py-3.5 rounded-sm">
+        <div className="connectModal fixed top-64 backdrop-blur-[1.5px] h-screen w-screen z-[100] hidden justify-center ">
+          <div className="modal bg-[#2e3a41] flex flex-col w-[20.75rem] h-[21.75rem] border border-[#5c666c] px-4 py-3.5 rounded-sm">
             <div className="flex justify-between items-center border-b border-b-[#5c666c] pb-3">
               <div className="flex gap-2 items-center">
                 <div className="w-5">
@@ -220,7 +255,7 @@ function Wallet() {
                 <img src={cross} className='w-full h-full object-cover' alt="" />
               </div>
             </div>
-            <div className="grid grid-cols-4 gap-x-3 gap-y-[25px] rounded-sm p-3 mt-3 w-[17.25rem] h-[15rem]">
+            <div className="grid grid-cols-4 gap-x-3 gap-y-[25px] rounded-sm p-3 mt-4 w-[18.75rem] h-[16.5rem]">
 
               {
                 walletData?.map((wData, index) => (
@@ -244,17 +279,17 @@ function Wallet() {
               <p className='font-semibold text-sm'>Welcome</p>
               <p className='capitalize text-[#a1a7aa] text-xs'>nimbithebest.xtsyvd</p>
             </div>
-            <div className="bg-[#2e3a41] border border-[#5c666c] flex flex-col gap-2.5 rounded-md font-['Inter'] w-full h-auto max-h-[675px] px-4 py-3.5">
+            <div className="bg-[#2e3a41] border border-[#5c666c] flex flex-col gap-2.5 rounded-md font-['Inter'] w-full h-auto max-h-[664px] px-4 py-3.5">
 
-              <div onClick={selectAll} className="flex items-center justify-end gap-2 w-[91%]">
+              <div onClick={selectAll} className="cursor-pointer flex items-center justify-end gap-2 w-[91%]">
                 <p className='text-sm'>select all</p>
                 <div className="w-4 h-4">
                   <img src={plus} alt="" />
                 </div>
               </div>
 
-              <div className=" bg-[#242d32] p-4 rounded-sm w-full md:w-[432px] h-[410px] px-3.5 py-3.5 ">
-                <div className="flex flex-col justify-center items-center gap-[4px] overflow-y-auto h-[382px]">
+              <div className=" bg-[#242d32] p-4 rounded-sm w-full md:w-[432px] h-[371px] px-3.5 py-3.5 ">
+                <div className="flex flex-col items-center justify-center h-full gap-[4px] overflow-y-auto ">
                   {
                     cryptoData.length > 0 && !loading &&
                     cryptoData?.map((crypto, index) => (
@@ -264,7 +299,7 @@ function Wallet() {
                             <img src={crypto.src} className='w-full h-full object-cover' alt="" />
                           </div>
                           <div className="flex flex-col text-left">
-                            <p className='text-xs'><span className='font-bold uppercase'>{crypto.symbol}</span> <span className='capitalize text-[#a1a7aa]'>{crypto.name.length > 4 ? crypto.name.slice(0, 4) + '...' : crypto.name}</span></p>
+                            <p className='text-xs'><span className='font-bold uppercase'>{crypto.symbol}</span> <span className='capitalize text-[#a1a7aa]'>{crypto.name.length > 8 ? crypto.name.slice(0, 6) + '...' : crypto.name}</span></p>
                             <p className='text-[10px]'>{crypto.amount > 0.01 ? crypto.amount.toFixed(3) : '<0.01'}</p>
                           </div>
                         </div>
@@ -293,7 +328,7 @@ function Wallet() {
 
               {
                 !loading &&
-                <div className="overflow-auto bg-[#242d32] p-4 rounded-sm w-full md:w-[432px] h-[148px] px-3.5 py-3.5 text-sm font-['Inter'] font-semibold flex flex-col justify-center gap-3">
+                <div className="bg-[#242d32] p-4 rounded-sm w-full md:w-[432px] h-[140px] px-3.5 py-3.5 text-sm font-['Inter'] font-semibold flex flex-col justify-center gap-3">
 
                   <div className="flex items-center justify-between">
                     <p className=''>Token</p>
@@ -336,9 +371,22 @@ function Wallet() {
           </div>
         }
 
+      <div className="capitalize 3xl:absolute bottom-0 w-full text-xs flex flex-col gap-4  px-[2rem] lg:px-[4rem] xl:px-[8rem] py-8">
+        <div className={`flex items-center gap-4 flex-col  justify-between 3xl:w-[90%] mx-auto`}>
+          <div className="flex flex-col items-center gap-1">
+            <p className='text-[#a1a7aa]'>our community</p>
+            <ul className="list-none flex gap-2 items-center">
+              <li><img src={telegramIcon} className='w-5 rounded-sm cursor-pointer' alt="" /></li>
+              <li><img src={discordIcon} className='w-5 rounded-sm cursor-pointer' alt="" /></li>
+              <li><img src={twitterIcon} className='w-5 rounded-sm cursor-pointer' alt="" /></li>
+              <li><img src={xIcon} className='w-5 rounded-sm cursor-pointer' alt="" /></li>
+            </ul>
+          </div>
+          <p className='normal-case'>Copyright &copy; 2024 NIMBI.COM | All rights reserved</p>
+        </div>
+      </div>
 
       </div>
-      <Footer path='/wallet' />
     </div>
   )
 }
